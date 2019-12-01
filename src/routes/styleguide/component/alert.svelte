7a93e02112330951b685aae3f787963edd6cae64
Wolfr <johan.ronsse@gmail.com>
2019-12-01 18:28:39 +0100

    Implement prism code examples
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- src/routes/styleguide/component/alert.svelte
+++ src/routes/styleguide/component/alert.svelte
@@ -1,7 +1,17 @@
 <script>
+
+    import Prism from 'svelte-prism';
+
     import Content from '../../../components/UI/Content.svelte';
     import Alert from '../../../components/UI/Alert.svelte';
     import Sample from '../../../components/Styleguide/Sample.svelte';
+
+    const defaultAlert = '<Alert type="default">I am a default alert.</Alert>';
+    const errorAlert = '<Alert type="error">I am an error message.</Alert>';
+    const successAlert = '<Alert type="success">I am a success message.</Alert>';
+    const alertTypeDefault = '<Alert type="default">Hey, I don\'t have any options.</Alert>';
+    const alertTypeDismissible = '<Alert type="default" dismissible>Hey, I am dismissible.</Alert>';
+
 </script>
 
 <Content>
@@ -12,14 +22,20 @@
     <Sample>
         <Alert type="default">I am a default alert.</Alert>
     </Sample>
+    <Prism language="html" source="{defaultAlert}" />
+
     <h3>Error</h3>
     <Sample>
         <Alert type="error">I am an error message.</Alert>
     </Sample>
+    <Prism language="html" source="{errorAlert}" />
+
+
     <h3>Success</h3>
     <Sample>
         <Alert type="success">I am a success message.</Alert>
     </Sample>
+    <Prism language="html" source="{errorAlert}" />
 
     <h2>Alert types</h2>
 
@@ -27,9 +43,13 @@
     <Sample>
         <Alert type="default">Hey, I don't have any options.</Alert>
     </Sample>
+    <Prism language="html" source="{alertTypeDefault}" />
+
     <h3>Dismissible</h3>
     <Sample>
         <Alert type="default" dismissible>Hey, I am dismissible.</Alert>
     </Sample>
+    <Prism language="html" source="{alertTypeDismissible}" />
 
 </Content>
+
--- src/routes/styleguide/component/blank-slate.svelte
+++ src/routes/styleguide/component/blank-slate.svelte
@@ -1,8 +1,14 @@
 <script>
+    import Prism from 'svelte-prism';
     import Blankslate from '../../../components/UI/Blankslate.svelte';
     import Sample from '../../../components/Styleguide/Sample.svelte';
 </script>
 
+
 <Sample>
     <Blankslate>I am a blank slate. Use me when no content is available.</Blankslate>
 </Sample>
+
+<h3>Implementation</h3>
+
+<Prism language="html" source="<Blankslate>I am a blank slate. Use me when no content is available.</Blankslate>" />
